2ff6e02a91d543310356d0c991985ff762aa5947
billtong <bill.tong@mitel.com>
2021-10-09 21:44:52 -0400

    Enable OS Data in LiveDashboard
    add :os_mon module

--- apps/server/mix.exs
+++ apps/server/mix.exs
@@ -20,7 +20,7 @@ defmodule Tetris.MixProject do
   def application do
     [
       mod: {Tetris.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
